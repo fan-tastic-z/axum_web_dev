ba1001b4ec082a097fd577ab5961757e9731cb55
fan-tastic <fan.zhao@chaitin.com>
2023-10-12 11:45:17 +0800

    feat: use base64 crate

--- examples/gen_key.rs
+++ examples/gen_key.rs
@@ -1,13 +1,14 @@
 use anyhow::Result;
+use base64::{engine::general_purpose, Engine};
 use rand::RngCore;
 
 fn main() -> Result<()> {
-    let mut key = [0u8; 64];
-    rand::thread_rng().fill_bytes(&mut key);
-    println!("\nGenrated key for HMAC:\n{key:?}");
+	let mut key = [0u8; 64];
+	rand::thread_rng().fill_bytes(&mut key);
+	println!("\nGenrated key for HMAC:\n{key:?}");
 
-    let b64u = base64_url::encode(&key);
-    println!("\nKey b64u encoded:\n{b64u}");
+	let b64u = general_purpose::URL_SAFE_NO_PAD.encode(key);
+	println!("\nKey b64u encoded:\n{b64u}");
 
-    Ok(())
+	Ok(())
 }
