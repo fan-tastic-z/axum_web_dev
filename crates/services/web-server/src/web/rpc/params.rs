5ae9a01fc0d0e7120824d4c5a981a528bfbdd791
fan-tastic-z <fantastic.fun.zf@gmail.com>
2023-11-09 13:29:10 +0800

    feat: rpc/params - added general implementations Option<IntoParams> and serde Value

--- crates/services/web-server/src/web/rpc/params.rs
+++ crates/services/web-server/src/web/rpc/params.rs
@@ -2,6 +2,7 @@ use crate::web::rpc::router::{IntoDefaultParams, IntoParams};
 use modql::filter::ListOptions;
 use serde::de::DeserializeOwned;
 use serde::Deserialize;
+use serde_json::Value;
 
 /// Params structure for any RPC Create call.
 #[derive(Deserialize)]
@@ -36,3 +37,29 @@ pub struct ParamsList<F> {
 
 impl<D> IntoDefaultParams for ParamsList<D> where D: DeserializeOwned + Send + Default
 {}
+
+// region:    --- General Implementations
+/// Implements `IntoParams` for any type that also implements `IntoParams`.
+///
+/// Note: Application code might prefer to avoid this blanket implementation
+///       and opt for enabling it on a per-type basis instead. If that's the case,
+///       simply remove this general implementation and provide specific
+///       implementations for each type.
+impl<D> IntoParams for Option<D>
+where
+	D: DeserializeOwned + Send,
+	D: IntoParams,
+{
+	fn into_params(value: Option<Value>) -> crate::web::Result<Self> {
+		let value = value.map(|v| serde_json::from_value(v)).transpose()?;
+		Ok(value)
+	}
+}
+
+/// This is the IntoParams implementation for serde_json Value.
+///
+/// Note: As above, this might not be a capability app code might want to
+///       allow for rpc_handlers, prefering to have everything strongly type.
+///       In this case, just remove this impelementation
+impl IntoParams for Value {}
+// endregion: --- General Implementations
